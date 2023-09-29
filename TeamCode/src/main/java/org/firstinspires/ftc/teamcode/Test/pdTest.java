173f582e3e38d965c7a3e1a198f9cf856f106cea
JusAryan <jusjot8@gmail.com>
2023-09-28 22:51:11 -0700

    Create pdTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/pdTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.Test;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name = "PD Test")
+public class pdTest extends LinearOpMode {
+
+    private DcMotorEx motorBackLeft;
+    private DcMotorEx motorBackRight;
+    private DcMotorEx motorFrontRight;
+    private DcMotorEx motorFrontLeft;
+
+    double previousError = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
+
+        motorFrontLeft = hardwareMap.get(DcMotorEx.class, "motorFrontLeft");
+        motorBackLeft = hardwareMap.get(DcMotorEx.class, "motorBackLeft");
+        motorFrontRight = hardwareMap.get(DcMotorEx.class, "motorFrontRight");
+        motorBackRight = hardwareMap.get(DcMotorEx.class, "motorBackRight");
+
+        motorFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        waitForStart();
+        while (opModeIsActive()) {
+            double power = PDController(0.002, 0.0005, 500, motorBackLeft.getCurrentPosition());
+
+
+            motorFrontRight.setPower(power);
+            motorFrontLeft.setPower(power);
+            motorBackRight.setPower(power);
+            motorBackLeft.setPower(power);
+        }
+    }
+
+    private double PDController(double proportionalGain, double derivativeGain, double setPoint, double processVariable) {
+
+        double error = setPoint - processVariable;
+        double pOut = proportionalGain * error;
+        double dOut = derivativeGain * (error - previousError);
+        previousError = error;
+
+
+        return pOut + dOut;
+    }
+}
