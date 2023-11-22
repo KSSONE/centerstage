045a8c29abf811b3fc3cd7bd5aea7b456cb16272
JoviSingh06 <singhjovanpreet60@gmail.com>
2023-11-22 13:18:28 -0800

    Update linearslide.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/linearslide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/linearslide.java
@@ -3,15 +3,14 @@ package org.firstinspires.ftc.teamcode.Test;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
-
 public class linearslide extends LinearOpMode {
     private DcMotor motor;
-    private DigitalChannel slideLimitSwitch;
+    //private DigitalChannel slideLimitSwitch;
 
     @Override
     public void runOpMode() {
         motor = hardwareMap.get(DcMotor.class, "motor");
-        slideLimitSwitch = hardwareMap.get(DigitalChannel.class, "slideLimitSwitch");
+        //slideLimitSwitch = hardwareMap.get(DigitalChannel.class, "slideLimitSwitch");
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -25,13 +24,14 @@ public class linearslide extends LinearOpMode {
                     motor.setPower(0.0);
                 }
             }
-            else if (gamepad1.x) {
-                if (slideLimitSwitch.getState() == false) {
+            else if (gamepad1.b) {
+                if (motor.getCurrentPosition() > 0) { //!slideLimitSwitch.getState()
                     motor.setPower(-0.2);
                 } else {
                     motor.setPower(0.0);
                 }
-            } else {
+            }
+            else {
                 motor.setPower(0.0);
             }
             telemetry.addData("Motor Position", motor.getCurrentPosition());
