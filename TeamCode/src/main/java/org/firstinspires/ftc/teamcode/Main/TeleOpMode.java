c8ad095d2a6c8d95848528375dc5d35d688c6e08
TwistedMiraculous <117871204+TwistedMiraculous@users.noreply.github.com>
2023-11-25 13:10:05 -0800

    TeleOp updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/TeleOpMode.java
@@ -7,71 +7,100 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 public class TeleOpMode extends LinearOpMode {
 
-    // Declare motor variables
-    private DcMotor frontLeft, frontRight, rearLeft, rearRight;
+    private DcMotor FLeft, FRight, BLeft, BRight;
+    private DcMotor slide;
 
     @Override
     public void runOpMode() {
-        // Initialize motors from hardware map
-        frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        frontRight = hardwareMap.dcMotor.get("frontRight");
-        rearLeft = hardwareMap.dcMotor.get("rearLeft");
-        rearRight = hardwareMap.dcMotor.get("rearRight");
+        // Wheels
+        FLeft = hardwareMap.dcMotor.get("FLeft");
+        FRight = hardwareMap.dcMotor.get("FRight");
+        BLeft = hardwareMap.dcMotor.get("BLeft");
+        BRight = hardwareMap.dcMotor.get("BRight");
 
-        // Set motor directions (depending on your wiring and wheel orientation)
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        rearLeft.setDirection(DcMotor.Direction.REVERSE);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-        rearRight.setDirection(DcMotor.Direction.FORWARD);
+        // Slide
+        slide = hardwareMap.dcMotor.get("slide");
 
-        // Set motor modes (RUN_USING_ENCODER if using encoders)
-        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rearLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rearRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Direction
+        FLeft.setDirection(DcMotor.Direction.REVERSE);
+        BLeft.setDirection(DcMotor.Direction.REVERSE);
+        FRight.setDirection(DcMotor.Direction.FORWARD);
+        BRight.setDirection(DcMotor.Direction.FORWARD);
 
-        // Wait for the start button to be pressed on the driver station
+        // Wheels
+        FLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        FRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        BRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Slide
+        slide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         waitForStart();
 
         while (opModeIsActive()) {
-            // Drive controls (use gamepad inputs for control)
-            double drive = -gamepad1.left_stick_y;
-            double strafe = gamepad1.left_stick_x;
-            double rotate = gamepad1.right_stick_x;
-
-            // Calculate motor powers for mecanum drive
-            double frontLeftPower = drive + strafe + rotate;
-            double frontRightPower = drive - strafe - rotate;
-            double rearLeftPower = drive - strafe + rotate;
-            double rearRightPower = drive + strafe - rotate;
-
-            // Normalize motor powers to stay within the [-1, 1] range
-            double maxPower = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower),
-                    Math.max(Math.abs(rearLeftPower), Math.abs(rearRightPower))));
-
-            if (maxPower > 1.0) {
-                frontLeftPower /= maxPower;
-                frontRightPower /= maxPower;
-                rearLeftPower /= maxPower;
-                rearRightPower /= maxPower;
-            }
-
-            // Set power to motors
-            frontLeft.setPower(frontLeftPower);
-            frontRight.setPower(frontRightPower);
-            rearLeft.setPower(rearLeftPower);
-            rearRight.setPower(rearRightPower);
-
-            // Other operations or telemetry can be added here for debugging
-
-            // Loop delay (optional)
-            sleep(20);
+            driveRobot();
+            linearslide();
         }
 
-        // Stop the motors when the OpMode is stopped
-        frontLeft.setPower(0);
-        frontRight.setPower(0);
-        rearLeft.setPower(0);
-        rearRight.setPower(0);
+        stopMotors();
+    }
+
+    private void driveRobot() {
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+
+        double frontLeftPower = drive + strafe + rotate;
+        double frontRightPower = drive - strafe - rotate;
+        double rearLeftPower = drive - strafe + rotate;
+        double rearRightPower = drive + strafe - rotate;
+
+        double maxPower = Math.max(
+                Math.abs(frontLeftPower),
+                Math.max(Math.abs(frontRightPower),
+                        Math.max(Math.abs(rearLeftPower), Math.abs(rearRightPower))));
+
+        if (maxPower > 1.0) {
+            frontLeftPower /= maxPower;
+            frontRightPower /= maxPower;
+            rearLeftPower /= maxPower;
+            rearRightPower /= maxPower;
+        }
+
+        FLeft.setPower(frontLeftPower);
+        FRight.setPower(frontRightPower);
+        BLeft.setPower(rearLeftPower);
+        BRight.setPower(rearRightPower);
+
+        sleep(20);
+    }
+
+    private void stopMotors() {
+        FLeft.setPower(0);
+        FRight.setPower(0);
+        BLeft.setPower(0);
+        BRight.setPower(0);
+    }
+    private void linearslide() {
+        if (gamepad1.a) {
+            if (slide.getCurrentPosition() < 3700) { // encoder resolution
+                slide.setPower(0.2);
+            } else {
+                slide.setPower(0.0);
+            }
+        }
+        else if (gamepad1.b) {
+            if (slide.getCurrentPosition() > 0) { //!slideLimitSwitch.getState()
+                slide.setPower(-0.2);
+            } else {
+                slide.setPower(0.0);
+            }
+        }
+        else {
+            slide.setPower(0.0);
+        }
+        telemetry.addData("Motor Position", slide.getCurrentPosition());
     }
 }
