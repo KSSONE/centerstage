d839630262b99f6a0afa9bc9c363b19025b9cd29
Sukhdeep Somal <117871204+TwistedMiraculous@users.noreply.github.com>
2024-01-15 14:54:48 -0800

    Update ODCM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ODCM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/ODCM.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Helpers;
+package org.firstinspires.ftc.teamcode.Main;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
